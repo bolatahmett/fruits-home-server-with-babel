5629699cd4b30aa4f05be3be935d7383bcff4eca
Ahmet BOLAT <bolatahmett@gmail.com>
2020-06-24 00:31:44 +0300

    add users

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use(cors());
 const dbUrl = 'mongodb+srv://fruitshomeonur:080905Ap@fruitshome-7laqp.gcp.mongodb.net/test?authSource=admin&replicaSet=FruitsHome-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true';
 const dbName = "FruitsHome";
 const collection = "Products";
+const userCollection = "Users";
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = urlencoded({ extended: false });
 // create application/json parser
@@ -98,11 +99,86 @@ app.post('/api/getAll', jsonParser, function (req, res) {
     }
 });
 
+
+
+
+app.post('/api/addUser', jsonParser, function (req, res) {
+    try {
+        MongoClient.connect(dbUrl, { useUnifiedTopology: true }, (err, client) => {
+            if (err)
+                throw err;
+            const db = client.db(dbName);
+            db.collection(userCollection).insertOne(req.body, (err, result) => {
+                if (err)
+                    throw err;
+                client.close();
+                res.send('success');
+            });
+        });
+    } catch (error) {
+        res.send(error);
+    }
+});
+// POST /api/users gets JSON bodies
+app.post('/api/getUser', jsonParser, function (req, res) {
+    try {
+        let result = {};
+        MongoClient.connect(dbUrl, { useUnifiedTopology: true }, (err, client) => {
+            if (err)
+                throw err;
+            const db = client.db(dbName);
+            db.collection(userCollection).findOne(req.body, (err, result) => {
+                if (err)
+                    throw err;
+                res.send(result);
+                client.close();
+            });
+        });
+    } catch (error) {
+        res.send(error);
+    }
+});
+// POST /api/users gets JSON bodies
+app.post('/api/removeUser', jsonParser, function (req, res) {
+    try {
+        MongoClient.connect(dbUrl, { useUnifiedTopology: true }, (err, client) => {
+            if (err)
+                throw err;
+            const db = client.db(dbName);
+            console.log(req.body);
+            db.collection(userCollection).deleteMany(req.body, (err, result) => {
+                if (err)
+                    throw err;
+                client.close();
+                res.send(result);
+            });
+        });
+    } catch (error) {
+        res.send(error);
+    }
+});
+app.post('/api/getAllUsers', jsonParser, function (req, res) {
+    try {
+        MongoClient.connect(dbUrl, { useUnifiedTopology: true }, (err, client) => {
+            if (err)
+                throw err;
+            const db = client.db(dbName);
+            db.collection(userCollection).find().toArray()
+                .then((docs) => res.send(docs));
+        });
+    } catch (error) {
+        res.send(error);
+    }
+});
+
+
+
+
 app.get('/api', (req, res) => {
     res.send("Server is running");
 })
 
 
-app.listen(3000, () => {
-    console.log(`app is listening to port 3000`);
+app.listen(5000, () => {
+    console.log(`app is listening to port 5000`);
 })
